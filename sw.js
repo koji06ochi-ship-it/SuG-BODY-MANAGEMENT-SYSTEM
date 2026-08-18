bba3705703c2b96b9d976403e0a3e18e5c7e41c9
koji06ochi-ship-it <koji06ochi@gmail.com>
2026-08-18 22:33:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sug-v24-4-invite-fix-network-only';
+const SW_VERSION = 'sug-v24-5-line-safe-network-only';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => { event.respondWith(fetch(event.request)); });
